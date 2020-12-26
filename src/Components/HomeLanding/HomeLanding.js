665da8d3dc46de4a8ac278cef43c75066e20f8b3
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-26 12:45:48 +0530

    Home-Image Added

---
+++ src/Components/HomeLanding/HomeLanding.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomeLanding.css';
+
+const HomeLanding = () => {
+    return (
+        <div class="pa4-l homeImg">
+            <div className='center'>
+                <div className="form center pa4 br3 shadow-5">
+                    <input className="f4 pa2 w-70 center" placeholder="Type Here" type="text"  />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg " >Search</button>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default HomeLanding;
