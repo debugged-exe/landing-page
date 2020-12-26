1741419870c635bf276bd498305e455c64eaba10
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-26 13:23:15 +0530

    Our Company Added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './Components/Navbar/nav.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer';
 import HomeLanding from './Components/HomeLanding/HomeLanding';
+import OurCompany from './Components/OurCompany/OurCompany';
 
 function App() {
   return (
     <div>
       <Navbar />
       <HomeLanding/>
+      <OurCompany/>
       <Footer />
     </div>
   );
