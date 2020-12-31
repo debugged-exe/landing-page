56d35ff89d215a8d6b22fa2b40f0f5cbf820affd
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-12-31 16:04:50 +0530

    responsiveness added

--- src/Components/HomeLanding/HomeLanding.js
+++ src/Components/HomeLanding/HomeLanding.js
@@ -3,7 +3,7 @@ import './HomeLanding.css';
 
 const HomeLanding = () => {
     return (
-        <div className="pa4-l homeImg">
+        <div className="homeImg">
             <div className='center'>
                 <div className="form center pa4 br3 shadow-5">
                     <input className="f4 pa2 w-70 center" placeholder="Type Here" type="text"  />
