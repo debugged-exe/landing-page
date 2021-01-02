0ee1b772a857768b829c0a18bad20de967bf64a1
imrehan <rehanshaikh2156@gmail.com>
2021-01-03 00:40:57 +0530

    privacy policy modal added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'tachyons';
 
-const Footer = () => {
+const Footer = ({showPolicy}) => {
     return (
         <footer style={{backgroundColor:'#15102c'}} className="b white-80 pv2">
             <p className="f5 flex flex-wrap justify-around mt3">
                 <span className="dib">©1999 BORA AUTOMOBILES, Inc.</span>
-                <a style={{textDecoration:'none',outline:'none'}} className="link white-80 hover-gray" href="/privacy"> Privacy Policy</a>
+                <a style={{textDecoration:'none',outline:'none'}} className="link white-80 hover-gray" onClick={showPolicy}> Privacy Policy</a>
                 <a style={{textDecoration:'none',outline:'none'}} className="link white-80 hover-gray" href="/#">boraautomobiles99@gmail.com </a>
             </p>
         </footer>
